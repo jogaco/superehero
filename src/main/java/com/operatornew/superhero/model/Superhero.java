f034d2f9caeb215b181eb2977631e5afb5a69b59
jogaco <>
2018-08-07 18:23:02 +0200

    Added date (de)serializer

--- src/main/java/com/operatornew/superhero/model/Superhero.java
+++ src/main/java/com/operatornew/superhero/model/Superhero.java
@@ -1,5 +1,10 @@
 package com.operatornew.superhero.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -16,6 +21,8 @@ public class Superhero {
     private String name;
 
     @Column
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate firstPublished;
 
     public Superhero() {}
---
+++ src/test/java/com/operatornew/superhero/model/SuperheroJsonTests.java
@@ -0,0 +1,33 @@
+package com.operatornew.superhero.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@JsonTest
+public class SuperheroJsonTests {
+
+    @Autowired
+    private JacksonTester<Superhero> jsonTester;
+
+    @Test
+    public void testSerialize() throws Exception {
+        Superhero superhero = new Superhero("name", "pseudonym");
+        superhero.setFirstPublished(LocalDate.of(2018,8,7));
+
+        String json = jsonTester.write(superhero).getJson();
+        assertThat(json, hasJsonPath("$.name", equalTo("name")));
+        assertThat(json, hasJsonPath("$.pseudonym", equalTo("pseudonym")));
+        assertThat(json, hasJsonPath("$.firstPublished", equalTo("2018-08-07")));
+    }
+}
